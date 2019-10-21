cbaf34f38cffaed812ad3d133c4e973438f5bbec
afazlagic1 <56510527+afazlagic1@users.noreply.github.com>
2019-10-21 15:59:05 +0200

    Update Artikl.java

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -1,4 +1,22 @@
 package ba.unsa.etf.rpr;
 
 public class Artikl {
+  private String imeart;
+  private int cijenaart;
+  private String kodart;
+  Artikal(String i, c, k) {
+    imeart = i; cijenaart = c; kodart = k;
+}
+  public Artikal(Artikal a) {
+      imeart = a.imeart; cijenaart = a.cijenaart; kodart = a.kodart;
+    }
+  public String getNaziv() {
+    return imeart;  
+  }
+  public int getCijena() {
+    return cijenaart; 
+  }
+  public String getKod() {
+    return kodart; 
+  }
 }
