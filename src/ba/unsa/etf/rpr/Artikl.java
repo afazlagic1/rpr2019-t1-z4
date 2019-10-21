8dc3e995525bc1a82149568dad7f5e525e0033d8
student <afazlagic1@etf.unsa.ba>
2019-10-21 17:51:38 +0200

    //

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -4,19 +4,19 @@ public class Artikl {
   private String imeart;
   private int cijenaart;
   private String kodart;
-  Artikal(String i, c, k) {
-    imeart = i; cijenaart = c; kodart = k;
+  Artikl(String imeart, int cijenaart, String kodart) {
+    this.imeart = imeart; this.cijenaart = cijenaart; this.kodart = kodart;
 }
-  public Artikal(Artikal a) {
+  public Artikl(Artikl a) {
       imeart = a.imeart; cijenaart = a.cijenaart; kodart = a.kodart;
     }
   public String getNaziv() {
-    return imeart;  
+    return imeart;
   }
   public int getCijena() {
     return cijenaart; 
   }
   public String getKod() {
-    return kodart; 
+    return this.kodart;
   }
 }
