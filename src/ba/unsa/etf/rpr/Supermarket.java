0befdca916be87407142a12d580185c1e5dad2f5
afazlagic1 <56510527+afazlagic1@users.noreply.github.com>
2019-10-21 16:37:44 +0200

    Update Supermarket.java

--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,28 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+  private int final maksUSupermarketu = 1000;
+  private Artikl[] artikl = new Artikl[maksUSupermarketu];
+  private int trenutnoUSupermarketu = 0;
+  public Artikl[] getArtikli() {
+    return artikl; 
+  }
+  void dodajArtikl(Artikl a) {
+    if(trenutnoUSupermarketu < maksUSupermarketu) {
+    artikl[trenutnoUSupermarketu++] = new Artikl(a);
+    }
+  }
+  public Artikl izbaciArtiklSaKodom(String kodart) {  //istafunkcijakaoukorpi
+    for(int i = 0; i < trenutnoUSupermarketu; i++) {
+       if(artikl[i]!=null && artikl[i].getKod().equals(kodart)) {
+        Artikl pomocni = artikl[i];
+        for(int j=i; j<trenutnoUSupermarketu - 1; j++) {
+          artikl[j] = artikl[j+1];
+      }
+        artikli[--trenutnoUSupermarketu] = null;
+        return pomocni;
+    }
+  }
+    return null;
+  }
 }
