f4a9f7b064e3df7fe53f2d6bb7f96e1954b2dc89
afazlagic1 <56510527+afazlagic1@users.noreply.github.com>
2019-10-21 16:19:02 +0200

    Update Korpa.java

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,39 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+  private int finale maksUKorpi = 50;
+  private Artikl[] artikl = new Artikl[maksUKorpi];
+  private int trenutnoUKorpi = 0;
+  public Artikl[] getArtikli() {
+    return artikl;
+  }
+  public boolean dodajArtikl(Artikl x) {
+    if(trenutnoUKorpi < maksUKorpi) {
+      artikl[trenutnoUKorpi++] = x;
+      return true;
+    }
+    return false;
+  }
+  public Artikl izbaciArtiklSaKodom(String kodart) {
+    for(int i = 0; i < trenutnoUKorpi; i++) {
+      if(artikl[i]!=null && artikl[i].getKod().equals(kodart)) {
+        Artikl pomocni = artikl[i];
+        for(int j=i; j<trenutnoUKorpi - 1; j++) {
+          artikl[j] = artikl[j+1];
+      }
+        artikli[trenutnoUKorpi--] = null;
+        return pomocni;
+    }
+  }
+    return null;
+  }
+  public int dajUkupnuCijenuArtikala() {
+    int suma = 0;
+    for(int i=0; i<trenutnoUKorpi; i++) {
+      if(artikl[i] != null) {
+        suma += artikl[i].getCijena();
+      }
+    }
+    return suma;
+  }
 }
